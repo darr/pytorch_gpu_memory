24b02f91f885c94c5d87af506c2e3116cda73afe
liuzy <lzygzh@126.com>
2019-06-02 17:19:21 +0800

    change

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # pytorch_gpu_memory
 pytorch gpu memory check
+
+## how to use gpu_memory_log
+
+```python
+import torch
+from gpu_memory_log import gpu_memory_log
+
+dtype = torch.float
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+device = torch.device("cuda")
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+
+learning_rate = 1e-6
+gpu_memory_log()
+for t in range(500):
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+    loss = (y_pred - y).pow(2).sum()
+    #print(t, loss.item())
+    loss.backward()
+    with torch.no_grad():
+        w1 -= learning_rate * w1.grad
+        w2 -= learning_rate * w2.grad
+
+        w1.grad.zero_()
+        w2.grad.zero_()
+
+print(loss.item())
+gpu_memory_log()
+```
+
+## how to run
+
+you can choose how to run.
+this will gpu_test.py
+```shell
+bash gpu_run.sh
+```
+
+output
+```shell
+LINE:27, FUNC:<module>, FILE:gpu_test.py, TIME:2019-06-02 17:16:23.288399, CONTENT:
+/home/liuzy/.pytorch_gpu_memory_env/py3env/lib/python3.5/site-packages/torch/distributed/distributed_c10d.py:100: UserWarning: torch.distributed.reduce_op is deprecated, please use torch.distributed.ReduceOp instead
+  warnings.warn("torch.distributed.reduce_op is deprecated, please use "
+[tensor: 1 * Size:(100, 10) | Memory: 0.0038 M | <class 'torch.Tensor'>]
+[tensor: 1 * Size:(64, 1000) | Memory: 0.2441 M | <class 'torch.Tensor'>]
+[tensor: 1 * Size:(64, 10) | Memory: 0.0024 M | <class 'torch.Tensor'>]
+[tensor: 1 * Size:(1000, 100) | Memory: 0.3814 M | <class 'torch.Tensor'>]
+memory_allocated:1.457520 Mb
+max_memory_allocated:1.457520 Mb
+memory_cached:2.000000 Mb
+max_memory_cached:2.000000 Mb
+Used Memory:9983.625000 Mb
+Free Memory:1185.625000 Mb
+Total Memory:11169.250000 Mb
+0.0032593868672847748
+LINE:41, FUNC:<module>, FILE:gpu_test.py, TIME:2019-06-02 17:16:23.791826, CONTENT:
+[tensor: 1 * Size:(100, 10) | Memory: 0.0038 M | <class 'torch.Tensor'>]
+[tensor: 1 * Size:() | Memory: 3.8146 M | <class 'torch.Tensor'>]
+[tensor: 1 * Size:(64, 1000) | Memory: 0.2441 M | <class 'torch.Tensor'>]
+[tensor: 2 * Size:(64, 10) | Memory: 0.0048 M | <class 'torch.Tensor'>]
+[tensor: 1 * Size:(1000, 100) | Memory: 0.3814 M | <class 'torch.Tensor'>]
+memory_allocated:1.846191 Mb
+max_memory_allocated:2.252930 Mb
+memory_cached:4.000000 Mb
+max_memory_cached:4.000000 Mb
+Used Memory:9989.625000 Mb
+Free Memory:1179.625000 Mb
+Total Memory:11169.250000 Mb
+```
+
+this will run gpu_test.ipynb, just open the jupyter
+```shell
+bash run.sh
+```
+
+
--- gpu_test.py
+++ gpu_test.py
@@ -3,134 +3,39 @@
 #####################################
 # File name : gpu_test.py
 # Create date : 2019-06-02 15:17
-# Modified date : 2019-06-02 17:06
+# Modified date : 2019-06-02 17:16
 # Author : DARREN
 # Describe : not set
 # Email : lzygzh@126.com
 #####################################
 from __future__ import division
 from __future__ import print_function
-import gc
-import datetime
-import pynvml
-import inspect
-
 import torch
-import numpy as np
-import sys
-
-def show(data):
-
-    print("data %x size:%s" % (id(data), str(data.size())))
-
-def get_tensors():
-    for obj in gc.get_objects():
-        try:
-
-            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
-                tensor = obj
-            else:
-                continue
-            if tensor.is_cuda:
-                show(tensor)
-                yield tensor
-        except Exception as e:
-            if False:
-                print('A trivial exception occured: {}'.format(e))
-def gpu_log():
-    stack_layer = 1
-    level = "DEBUG"
-    func_name = sys._getframe(stack_layer).f_code.co_name
-    file_name = sys._getframe(stack_layer).f_code.co_filename
-    line = sys._getframe(stack_layer).f_lineno
-    now_time = datetime.datetime.now()
-    log_format = 'LEVEL:%s, LINE:%s, FUNC:%s, FILE:%s, TIME:%s, CONTENT:%s\n'
-    #con = log_format % (level, line, func_name, file_name, now_time, content)
-    
-    pynvml.nvmlInit()
-    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-    meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-
-    #self.curr_line = self.frame.f_lineno
-    #where_str = self.module_name + ' ' + self.func_name + ':' + ' line ' + str(self.curr_line)
-    gpu_profile_fn = "" + 'gpu_mem_track.txt'
-
-    with open(gpu_profile_fn, 'a+') as f:
-        f.write("Total Used Memory:%f Mb\n\n" % float(meminfo.used/1024**2))
-        ts_list = [tensor.size() for tensor in get_tensors()]
-        new_tensor_sizes = {(type(x), tuple(x.size()), ts_list.count(x.size()), np.prod(np.array(x.size()))*4/1000**2)
-                            for x in get_tensors()}
-        for t, s, n, m in new_tensor_sizes:
-            f.write('| %s * Size:%s | Memory: %s M | %s\n' %(str(n), str(s), str(m*n)[:6], str(t)))
-
-        content = "%s" % str(meminfo.used/1024**2)
-            
-        con = log_format % (level, line, func_name, file_name, now_time, content)
-
-        #f.write("\nAt %s Total Used Memory:%f Mb\n\n" % (where_str, ))
-        f.write("\n%s" % con)
-
-    pynvml.nvmlShutdown()
-import torch
+from gpu_memory_log import gpu_memory_log
 
 dtype = torch.float
-
-
 N, D_in, H, D_out = 64, 1000, 100, 10
 
-
-#frame = inspect.currentframe()          # define a frame to track
-#gpu_tracker = MemTracker(frame)         # define a GPU tracker
-
-#gpu_log()
 device = torch.device("cuda")
-x = torch.randn(N, D_in, device=device, dtype=dtype)
-y = torch.randn(N, D_out, device=device, dtype=dtype)
-device = torch.device("cpu")
 x = torch.randn(N, D_in, device=device, dtype=dtype)
 y = torch.randn(N, D_out, device=device, dtype=dtype)
 
 w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
 w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
 
-
 learning_rate = 1e-6
+gpu_memory_log()
 for t in range(500):
-
-    h = x.mm(w1)
-    h2 = h.clamp(min=0)
-    y_pred = h2.mm(w2)
-    #y_pred = x.mm(w1).clamp(min=0).mm(w2)
-
-    # Compute and print loss using operations on Tensors.
-    # Now loss is a Tensor of shape (1,)
-    # loss.item() gets the a scalar value held in the loss.
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
     loss = (y_pred - y).pow(2).sum()
-    print(t, loss.item())
-
-    # Use autograd to compute the backward pass. This call will compute the
-    # gradient of loss with respect to all Tensors with requires_grad=True.
-    # After this call w1.grad and w2.grad will be Tensors holding the gradient
-    # of the loss with respect to w1 and w2 respectively.
+    #print(t, loss.item())
     loss.backward()
-
-
     with torch.no_grad():
-
-
-        show(w1)
-        show(w2)
-        w1_grad = w1.grad
-        w2_grad = w2.grad
-        show(w1_grad)
-        show(w2_grad)
         w1 -= learning_rate * w1.grad
-
         w2 -= learning_rate * w2.grad
-
-
 
         w1.grad.zero_()
         w2.grad.zero_()
 
-    gpu_log()
+print(loss.item())
+gpu_memory_log()
